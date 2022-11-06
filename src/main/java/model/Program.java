f24ef6d07e13e872b2d6cbe87001fcf71c399376
plgborowiczm <mikolajborowicz7@gmail.com>
2022-11-06 13:04:49 +0100

    simple-extension

--- src/main/java/model/Program.java
+++ src/main/java/model/Program.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Program extends Node {
 
+    private int maxDepth;
+
     public Program(Node parentNode, String name, boolean isCrossable) {
         super(parentNode, name, isCrossable);
     }
